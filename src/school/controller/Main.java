744c75a2051ba7b5289fe04bb2e94d548502f613
Dmytro <dmytro.andrusiv@oe.if.ua>
2016-11-17 09:36:28 +0200

    fixed method for make schedule

--- src/school/controller/Main.java
+++ src/school/controller/Main.java
@@ -6,92 +6,56 @@ import school.service.ServiceDirector;
 import school.service.ServiceStudent;
 
 public class Main {
-	final static int STUDENT_ROLE = 1;
-	final static int EDUCATOR_ROLE = 2;
-	final static int DIRECTOR_ROLE = 3;
+  final static int STUDENT_ROLE = 1;
+  final static int EDUCATOR_ROLE = 2;
+  final static int DIRECTOR_ROLE = 3;
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		DbInitial.initialUser();
-		DbInitial.initialALLSubject();
-		DbInitial.initialAudience();
-		DbInitial.initialClases();
-		ServiceStudent st = new ServiceStudent();
+    DbInitial.initialUser();
+    DbInitial.initialALLSubject();
+    DbInitial.initialAudience();
+    DbInitial.initialClases();
+    ServiceStudent st = new ServiceStudent();
 
-		ServiceDirector sd = new ServiceDirector();
+    ServiceDirector sd = new ServiceDirector();
 
-		// test code
-		// List<Subject> test = new ArrayList<Subject>();
-		// test.add(Db.subjects.get(5));
-		// test.add(Db.subjects.get(1));
-		// test.add(Db.subjects.get(2));
-		// test.add(Db.subjects.get(3));
-		// test.add(Db.subjects.get(4));
-		// test.add(Db.subjects.get(3));
-		// test.add(Db.subjects.get(2));
-		// test.add(Db.subjects.get(1));
-		// test.add(Db.subjects.get(3));
-		// test.add(Db.subjects.get(2));
-		// test.add(Db.subjects.get(3));
-		// test.add(Db.subjects.get(5));
-		// test.add(Db.subjects.get(2));
-		// test.add(Db.subjects.get(2));
-		// test.add(Db.subjects.get(4));
-		// test.add(Db.subjects.get(1));
-		//
-		// System.out.println("test code : ");
-		// sd.createSchedule(test, 1);
-		//
-		// for (int i = 0; i < Db.schedule.size(); i++) {
-		// ArrayList<LessonForSchedule> list = Db.schedule.get(i);
-		// System.out.println("Size : " + Db.schedule.get(i).size());
-		// for (int j = 0; j < list.size(); j++) {
-		// // LessonForSchedule lfs = list.get(j);
-		// System.out.print("N: " + list.get(j).getNumbLesson() + " " +
-		// list.get(j).getAudience() + " " + list.get(j).getNameLesson() + " ");
-		// System.out.println();
-		// }
-		//
-		// }
+    while (true) {
+      st.hellowPages();
+      int role = st.autentification();
 
-		//
+      if (role == STUDENT_ROLE) {
+        String nameUser = Db.currentSession.getUser().getName();
+        System.out.println("Dear " + nameUser);
+        System.out.println("hello in our programm!!!!");
+      }
+      if (role == EDUCATOR_ROLE) {
+        String nameEducator = Db.currentSession.getUser().getName();
+        System.out.println("DEAR " + nameEducator);
+      }
+      if (role == DIRECTOR_ROLE) {
+        String nameDirector = Db.currentSession.getUser().getName();
+        System.out.println("DEAR " + nameDirector);
+        boolean start = true;
+        int parametr;
 
-		while (true) {
-			st.hellowPages();
-			int role = st.autentification();
-
-			if (role == STUDENT_ROLE) {
-				String nameUser = Db.currentSession.getUser().getName();
-				System.out.println("Dear " + nameUser);
-				System.out.println("hello in our programm!!!!");
-			}
-			if (role == EDUCATOR_ROLE) {
-				String nameEducator = Db.currentSession.getUser().getName();
-				System.out.println("DEAR " + nameEducator);
-			}
-			if (role == DIRECTOR_ROLE) {
-				String nameDirector = Db.currentSession.getUser().getName();
-				System.out.println("DEAR " + nameDirector);
-				boolean start = true;
-				int parametr;
-
-				while (start) {
-					System.out.println("Shoose service : ");
-					System.out.println("create shedule press : 2");
-					System.out.println("show shedule press : 1");
-					parametr = Integer.parseInt(sd.enterNumber());
-					if (parametr == 2) {
-						sd.shooseSubjectForCreateShedule();
-					} else if (parametr == 1) {
-						sd.showAllSсhedule();
-					}
-					System.out.println("If you want yet press : 1");
-					parametr = Integer.parseInt(sd.enterNumber());
-					if (parametr != 1) {
-						start = false;
-					}
-				}
-			}
-		}
-	}
+        while (start) {
+          System.out.println("Shoose service : ");
+          System.out.println("create shedule press : 2");
+          System.out.println("show shedule press : 1");
+          parametr = Integer.parseInt(sd.enterNumber());
+          if (parametr == 2) {
+            sd.shooseSubjectForCreateShedule();
+          } else if (parametr == 1) {
+            sd.showAllSchedule();
+          }
+          System.out.println("If you want yet press : 1");
+          parametr = Integer.parseInt(sd.enterNumber());
+          if (parametr != 1) {
+            start = false;
+          }
+        }
+      }
+    }
+  }
 }
--- src/school/service/ServiceDirector.java
+++ src/school/service/ServiceDirector.java
@@ -11,213 +11,234 @@ import school.model.Subject;
 
 public class ServiceDirector extends ServiceStudent {
 
-	private Random random = new Random();
-	private final int MONDAY = 0;
-	private final int TUESDAY = 1;
-	private final int WEDNESDAY = 2;
-	private final int THURSDAY = 3;
-	private final int FRIDAY = 4;
-	private ArrayList<LessonForSchedule> mondey = new ArrayList<LessonForSchedule>();
-	private ArrayList<LessonForSchedule> tuesday = new ArrayList<LessonForSchedule>();
-	private ArrayList<LessonForSchedule> wednesday = new ArrayList<LessonForSchedule>();
-	private ArrayList<LessonForSchedule> thursday = new ArrayList<LessonForSchedule>();
-	private ArrayList<LessonForSchedule> friday = new ArrayList<LessonForSchedule>();
+  private Random random = new Random();
+  private final int MONDAY = 0;
+  private final int TUESDAY = 1;
+  private final int WEDNESDAY = 2;
+  private final int THURSDAY = 3;
+  private final int FRIDAY = 4;
+  private ArrayList<LessonForSchedule> mondey;
+  private ArrayList<LessonForSchedule> tuesday;
+  private ArrayList<LessonForSchedule> wednesday;
+  private ArrayList<LessonForSchedule> thursday;
+  private ArrayList<LessonForSchedule> friday;
 
-	List<LessonForSchedule> listLessonForAllClassesDay;
-	List<Subject> subjectsForSchedule;
-	List<Subject> listSubjects = new ArrayList<Subject>();
+  List<LessonForSchedule> listLessonForAllClassesDay;
+  List<Subject> subjectsForSchedule;
+  List<Subject> listSubjects = new ArrayList<Subject>();
 
-	public void createSchedule(int clas) {
-		System.out.println("List subject size : " + listSubjects.size());
+  public void createSchedule(int clas) {
+    mondey = new ArrayList<LessonForSchedule>();
+    tuesday = new ArrayList<LessonForSchedule>();
+    wednesday = new ArrayList<LessonForSchedule>();
+    thursday = new ArrayList<LessonForSchedule>();
+    friday = new ArrayList<LessonForSchedule>();
 
-		subjectsForSchedule = listSubjects;
+    System.out.println("List subject size : " + listSubjects.size());
 
-		int numbOfAllLessons = listSubjects.size();
+    subjectsForSchedule = listSubjects;
 
-		int startDayOfWeek = MONDAY;
-		int countLessonInDay = 0;
+    int numbOfAllLessons = listSubjects.size();
 
-		for (int i = 0; i < numbOfAllLessons; i++) {
+    int startDayOfWeek = MONDAY;
+    int countLessonInDay = 1;
 
-			int iterAudiance = random.nextInt(Db.audiences.size());
-			int numbAudiance = Db.audiences.get(iterAudiance).getNumber();
-			int iterSubject = random.nextInt(subjectsForSchedule.size());
-			String nameSubject = subjectsForSchedule.get(iterSubject).getName();
+    for (int i = 0; i < numbOfAllLessons; i++) {
 
-			LessonForSchedule lessons = new LessonForSchedule(countLessonInDay, clas, numbAudiance, nameSubject);
-			if (startDayOfWeek >= 4) {
-				countLessonInDay += 1;
-				startDayOfWeek = 0;
-			}
-			if (startDayOfWeek == MONDAY) {
-				mondey.add(getGoodLesson(lessons, startDayOfWeek));
-				startDayOfWeek++;
-				continue;
-			}
-			if (startDayOfWeek == TUESDAY) {
-				tuesday.add(getGoodLesson(lessons, startDayOfWeek));
-				startDayOfWeek++;
-				continue;
-			}
-			if (startDayOfWeek == WEDNESDAY) {
-				wednesday.add(getGoodLesson(lessons, startDayOfWeek));
-				startDayOfWeek++;
-				continue;
-			}
-			if (startDayOfWeek == THURSDAY) {
-				thursday.add(getGoodLesson(lessons, startDayOfWeek));
-				startDayOfWeek++;
-				continue;
-			}
-			if (startDayOfWeek == FRIDAY) {
-				friday.add(getGoodLesson(lessons, startDayOfWeek));
-				startDayOfWeek++;
-				continue;
-			}
-		}
+      int iterAudiance = random.nextInt(Db.audiences.size());
+      int numbAudiance = Db.audiences.get(iterAudiance).getNumber();
+      int iterSubject = random.nextInt(subjectsForSchedule.size());
+      String nameSubject = subjectsForSchedule.get(iterSubject).getName();
 
-		Db.schedule.add(MONDAY, mondey);
-		Db.schedule.add(TUESDAY, tuesday);
-		Db.schedule.add(WEDNESDAY, wednesday);
-		Db.schedule.add(THURSDAY, thursday);
-		Db.schedule.add(FRIDAY, friday);
+      if (startDayOfWeek > 4) {
+        countLessonInDay += 1;
+        startDayOfWeek = 0;
+      }
+      LessonForSchedule lessons = new LessonForSchedule(countLessonInDay, clas, numbAudiance, nameSubject);
 
-	}
+      if (startDayOfWeek == MONDAY) {
+        mondey.add(getGoodLesson(lessons, startDayOfWeek));
+        startDayOfWeek++;
+        continue;
+      }
+      if (startDayOfWeek == TUESDAY) {
+        tuesday.add(getGoodLesson(lessons, startDayOfWeek));
+        startDayOfWeek++;
+        continue;
+      }
+      if (startDayOfWeek == WEDNESDAY) {
+        wednesday.add(getGoodLesson(lessons, startDayOfWeek));
+        startDayOfWeek++;
+        continue;
+      }
+      if (startDayOfWeek == THURSDAY) {
+        thursday.add(getGoodLesson(lessons, startDayOfWeek));
+        startDayOfWeek++;
+        continue;
+      }
+      if (startDayOfWeek == FRIDAY) {
+        friday.add(getGoodLesson(lessons, startDayOfWeek));
+        startDayOfWeek++;
+        continue;
+      }
+    }
 
-	public LessonForSchedule getGoodLesson(LessonForSchedule less, int day) {
-		int lessCount = less.getNumbLesson();
-		int lessAudience = less.getAudience();
-		String lessName = less.getNameLesson();
+    Db.schedule.add(MONDAY, mondey);
+    Db.schedule.add(TUESDAY, tuesday);
+    Db.schedule.add(WEDNESDAY, wednesday);
+    Db.schedule.add(THURSDAY, thursday);
+    Db.schedule.add(FRIDAY, friday);
 
-		if (Db.schedule.size() == 0) {
-			return less;
+  }
 
-		} else {
+  public LessonForSchedule getGoodLesson(LessonForSchedule less, int day) {
+    int lessCount = less.getNumbLesson();
+    int lessAudience = less.getAudience();
+    String lessName = less.getNameLesson();
 
-			listLessonForAllClassesDay = Db.schedule.get(day);
+    if (Db.schedule.size() == 0) {
+      deleteSubjectFromList(less.getNameLesson());
+      return less;
 
-			boolean start = true;
+    } else {
 
-			while (start) {
-				int countEqualAudiance = 0;
-				int countEqualLessName = 0;
+      listLessonForAllClassesDay = Db.schedule.get(day);
 
-				for (int i = 0; i < listLessonForAllClassesDay.size(); i++) {
-					int lessCountList = listLessonForAllClassesDay.get(i).getNumbLesson();
-					int lessAudianceList = listLessonForAllClassesDay.get(i).getAudience();
-					String lessNameList = listLessonForAllClassesDay.get(i).getNameLesson();
+      boolean start = true;
 
-					if (lessAudience == lessAudianceList) {
-						countEqualAudiance++;
+      while (start) {
+        int countEqualAudiance = 0;
+        int countEqualLessName = 0;
 
-					}
-					if (lessCount == lessCountList && lessName.equals(lessNameList)) {
-						countEqualLessName++;
+        for (int i = 0; i < listLessonForAllClassesDay.size(); i++) {
+          int lessCountList = listLessonForAllClassesDay.get(i).getNumbLesson();
+          int lessAudianceList = listLessonForAllClassesDay.get(i).getAudience();
+          String lessNameList = listLessonForAllClassesDay.get(i).getNameLesson();
 
-					}
-				}
-				if (countEqualAudiance > 0) {
-					int iterAudiance = random.nextInt(Db.audiences.size());
-					lessAudience = Db.audiences.get(iterAudiance).getNumber();
-				}
-				if (countEqualLessName > 0) {
-					int iterNameLess = random.nextInt(subjectsForSchedule.size());
-					lessName = subjectsForSchedule.get(iterNameLess).getName();
-				}
-				if (countEqualAudiance == 0 && countEqualLessName == 0) {
-					start = false;
-					less.setNameLesson(lessName);
-					less.setAudience(lessAudience);
-					deleteSubjectFromList(less.getNameLesson());
-				}
-			}
-		}
-		return less;
-	}
+          if (lessAudience == lessAudianceList) {
+            countEqualAudiance++;
 
-	public void deleteSubjectFromList(String name) {
-		Iterator<Subject> iter = subjectsForSchedule.iterator();
-		while (iter.hasNext()) {
-			Subject s = iter.next();
-			if (name.equals(s.getName())) {
-				iter.remove();
-				break;
-			}
-		}
-	}
+          }
+          if (lessCount == lessCountList && lessName.equals(lessNameList)) {
+            countEqualLessName++;
 
-	public void shooseSubjectForCreateShedule() {
-		listSubjects = new ArrayList<Subject>();
-		System.out.println("Make shoose subjects for make shedule : ");
-		System.out.println("Fist shoose the numb of clas : ");
-		System.out.println("List clases : ");
-		viewAllClases();
-		int clas = Integer.parseInt(enterNumber());
-		System.out.println("List subjects : ");
-		viewAllSubjects();
-		boolean start = true;
-		while (start) {
-			System.out.println("Enter number subject : ");
-			int numbOfSubject = Integer.parseInt(enterNumber());
-			System.out.println("Enter count subject : ");
-			int countSubjects = Integer.parseInt(enterNumber());
-			addSubjectsorShedule(numbOfSubject, countSubjects);
-			System.out.println("if you want add more subjects press : 1");
-			System.out.println("if it all press : same key ");
-			int parametr = Integer.parseInt(enterNumber());
-			if (parametr != 1) {
-				start = false;
-			}
-		}
-		createSchedule(clas);
-	}
+          }
+        }
+        if (countEqualAudiance > 0) {
+          int iterAudiance = random.nextInt(Db.audiences.size());
+          lessAudience = Db.audiences.get(iterAudiance).getNumber();
+        }
+        if (countEqualLessName > 0) {
+          int iterNameLess = random.nextInt(subjectsForSchedule.size());
+          lessName = subjectsForSchedule.get(iterNameLess).getName();
+        }
+        if (countEqualAudiance == 0 && countEqualLessName == 0) {
+          start = false;
+          less.setNameLesson(lessName);
+          less.setAudience(lessAudience);
+          deleteSubjectFromList(less.getNameLesson());
+        }
+      }
+    }
+    return less;
+  }
 
-	public void viewAllSubjects() {
-		for (int i = 0; i < Db.subjects.size(); i++) {
-			String name = Db.subjects.get(i).getName();
-			System.out.println("N: " + i + " " + name);
+  public void deleteSubjectFromList(String name) {
+    Iterator<Subject> iter = subjectsForSchedule.iterator();
+    while (iter.hasNext()) {
+      Subject s = iter.next();
+      if (name.equals(s.getName())) {
+        iter.remove();
+        break;
+      }
+    }
+  }
 
-		}
-	}
+  public void shooseSubjectForCreateShedule() {
+    listSubjects = new ArrayList<Subject>();
+    System.out.println("Make shoose subjects for make shedule : ");
+    System.out.println("Fist shoose the numb of clas : ");
+    System.out.println("List clases : ");
+    viewAllClases();
+    int clas = Integer.parseInt(enterNumber());
+    System.out.println("List subjects : ");
+    viewAllSubjects();
+    boolean start = true;
+    while (start) {
+      System.out.println("Enter number subject : ");
+      int numbOfSubject = Integer.parseInt(enterNumber());
+      System.out.println("Enter count subject : ");
+      int countSubjects = Integer.parseInt(enterNumber());
+      addSubjectsorShedule(numbOfSubject, countSubjects);
+      System.out.println("if you want add more subjects press : 1");
+      System.out.println("if it all press : same key ");
+      int parametr = Integer.parseInt(enterNumber());
+      if (parametr != 1) {
+        start = false;
+      }
+    }
+    createSchedule(clas);
+  }
 
-	public void viewAllClases() {
-		for (int i = 0; i < Db.clases.size(); i++) {
-			int name = Db.clases.get(i).getNumb();
-			System.out.println("- " + name);
-		}
-	}
+  public void viewAllSubjects() {
+    for (int i = 0; i < Db.subjects.size(); i++) {
+      String name = Db.subjects.get(i).getName();
+      System.out.println("N: " + i + " " + name);
 
-	public void addSubjectsorShedule(int numbSubject, int countSubject) {
-		for (int i = 0; i < countSubject; i++) {
-			for (int j = 0; j < Db.subjects.size(); j++) {
-				Subject sub = Db.subjects.get(j);
-				if (numbSubject == j) {
-					listSubjects.add(sub);
-				}
-			}
-		}
-	}
+    }
+  }
 
-	public void showAllSсhedule() {
-		for (int i = 0; i < Db.clases.size(); i++) {
-			int numbOfClass = Db.clases.get(i).getNumb();
-			System.out.println("-|--|---|--|--|---|-----|---|----|---");
-			System.out.println("class : " + numbOfClass);
-			for (int j = 0; j < Db.schedule.size(); j++) {
-				System.out.println("--------------");
-				System.out.println("day : " + j);
-				System.out.println("--------------");
-				for (int j2 = 0; j2 < Db.schedule.get(j).size(); j2++) {
-					LessonForSchedule lessonForSchedule = Db.schedule.get(j).get(j2);
-					int clasLesson = lessonForSchedule.getClas();
-					int countLesson = lessonForSchedule.getNumbLesson();
-					int numbAudience = lessonForSchedule.getAudience();
-					String name = lessonForSchedule.getNameLesson();
-					if (numbOfClass == clasLesson) {
-						System.out.println("№: " + countLesson + " " + numbAudience + " " + name);
-					}
-				}
-			}
-		}
-	}
+  public void viewAllClases() {
+    for (int i = 0; i < Db.clases.size(); i++) {
+      int name = Db.clases.get(i).getNumb();
+      System.out.println("- " + name);
+    }
+  }
+
+  public void addSubjectsorShedule(int numbSubject, int countSubject) {
+    for (int i = 0; i < countSubject; i++) {
+      for (int j = 0; j < Db.subjects.size(); j++) {
+        Subject sub = Db.subjects.get(j);
+        if (numbSubject == j) {
+          listSubjects.add(sub);
+        }
+      }
+    }
+  }
+
+  public void showAllSchedule() {
+    int countDay;
+    int day = 1;
+    for (int i = 0; i < Db.clases.size(); i++) {
+      int numbOfClass = Db.clases.get(i).getNumb();
+      System.out.println("-|--|---|--|--|---|-----|---|----|---");
+      System.out.println("class : " + numbOfClass);
+      for (int j = 0; j < Db.schedule.size(); j++) {
+        countDay = j;
+        for (int j2 = 0; j2 < Db.schedule.get(j).size(); j2++) {
+
+          LessonForSchedule lessonForSchedule = Db.schedule.get(j).get(j2);
+          int clasLesson = lessonForSchedule.getClas();
+          int countLesson = lessonForSchedule.getNumbLesson();
+          int numbAudience = lessonForSchedule.getAudience();
+          String name = lessonForSchedule.getNameLesson();
+          if (numbOfClass == clasLesson) {
+            if (countDay == j) {
+
+              System.out.println("--------------");
+              System.out.println(day + " day : ");
+              System.out.println("--------------");
+              day++;
+              if (day > 5) {
+                day = 1;
+              }
+            }
+
+            System.out.println("N: " + countLesson + " " + numbAudience + " " + name);
+            countDay++;
+          }
+        }
+      }
+    }
+  }
 }
